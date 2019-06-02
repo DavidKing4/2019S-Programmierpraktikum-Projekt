23c44a364c728208837a828ab2760cb917f650d6
ubDem <utkubilendemir@gmail.com>
2019-06-02 23:51:39 +0200

    Trie-Method added, but it works too slow somehow.

--- Words.py
+++ Words.py
@@ -1,18 +1,10 @@
-import pytrie as py
-import os
-
-
-# The working directory
-# os.getcwd()
-
-# Um "working directory" zu ändern | An dieser Stelle müssen wir eine Lösung finden direkt die Datei im Github-Ordner zu addressieren.
-# os.chdir("C:/Users/Utku Bilen Demir/Desktop/e4Ke2/Math/Programmieren/ProProjekt/2019S-Programmierpraktikum-Projekt")
+from pytrie import SortedStringTrie as baum
 
 
 class Words:
     """ Lese alle Wörter in eine Liste ein """
 
-    def __init__(self, path='words_alpha.txt'):  # Wir können später optimisieren
+    def __init__(self, path='words_new.txt'):  # Wir können später optimisieren
         with open(path) as word_file:
             valid_words = list(word_file.read().split())
 
@@ -23,53 +15,28 @@ class Words:
 
     """Die Länge des längsten Wortes"""
 
-    def long_boi(
-            self):  # Das längste Wort: 'dichlorodiphenyltrichloroethane' mit einer Länge von 31 Buchstaben
+    def long_boi(self):  # Das längste Wort: 'dichlorodiphenyltrichloroethane' mit einer Länge von 31 Buchstaben
         lon = ''
         for i in self.list:
             if len(i) > len(lon):
                 lon = i
-        return (len(lon))
+        return len(lon)
 
-    """ Trei Versuch - PRÄFIX-BASIERT | Es hat nicht funktioniert aber ich versuch später weiter """
+    """ Trei Versuch - PRÄFIX-BASIERT | Funktioniert aber nicht wie ich will """
 
-    def trei(self, pre):
-        tr = py.StringTrie()  # Ein leeres Bäumchen
-        for i in self:  # Unsere Liste wird ins Trei eingetragen
-            tr[i] = i
-        return tr.values(pre)  # Passende Erweiterungen werden ausgegeben.
+    def trie(self):
+        di = {}
+        for k in range(len(self.list)-1):
+            di[self.list[k]] = k
+
+        return baum(di)
 
 
 """ TESTS """
 
-# Words.trei(w,'a')
+# Trie Test
+w = Words()
+tr = Words.trie(w)
+tr.keys(prefix="aman")
 
 
-# Words.long_boi(w)
-
-# w = Words()
-# print(w)
-
-# w.list[300000]
-
-# w.list[len(w.list)-1]
-
-
-# for i in wlist:
-#     if i[1] == 'a':
-#         return i
-
-
-# lon = ''
-# for i in w.list:
-#     if len(i) > len(lon):
-#         lon= i
-
-
-# lon
-
-
-# py.Trie(w.list)
-
-
-# py.Trie()
