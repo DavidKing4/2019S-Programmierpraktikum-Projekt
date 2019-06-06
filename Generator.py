b237c9c102f65f3f966596eb60d7db7311a6ed51
ubDem <utkubilendemir@gmail.com>
2019-06-06 17:16:31 +0200

    Added 'hood' attribute to extract the neighboring letters.

--- Generator.py
+++ Generator.py
@@ -4,14 +4,29 @@ import random
 import string
 
 class Board:
+    # We need to prevent the repetitions
 
-	def __init__(self, n = 4):    # Dynamic range with "n", in case we decide to alter the size of the board
-		lettersString = string.ascii_letters[:26] #'abcdefghijklmnopqrstuvwxyz'
-		boardLetters = [['' for i in range(n)] for j in range(n)]
-		for i in range(n):
-			for j in range(n):
-				boardLetters[i][j] = random.choice(lettersString)
-		self.letters = boardLetters
+    def __init__(self, n = 4):    # Dynamic range with "n", in case we decide to alter the size of the board
+        lettersString = string.ascii_letters[:26]    # 'abcdefghijklmnopqrstuvwxyz'
+        boardLetters = [['' for i in range(n)] for j in range(n)]
+        for i in range(n):
+            for j in range(n):
+                boardLetters[i][j] = random.choice(lettersString)
+        self.letters = boardLetters
 
-	def __str__(self):
-		return(str(self.letters))
+    def __str__(self):
+        return str(self.letters)
+
+    """ Benachbarte Buchstaben von einem beliebigen Buchstabe """
+    def hood(self, row, col):
+        # will be optimised
+
+        le_pos = self.letters[row][col]
+        hoodies = []
+        for i in range(row-1, row+2):
+            for j in range(row-1, col+2):
+                hoodies.append(self.letters[i][j])
+        self.hoodies = hoodies
+
+
+
--- Test.py
+++ Test.py
@@ -1,9 +1,43 @@
-from Generator import Board
+import Generator
 from Words import Words
 
-b = Board()
+b = Generator.Board()
 print(b)
 
-print(Board(n=6))
+print(Generator.Board(n=6))
 
 print(Words('words_new.txt').list[0])
+
+
+bo = Generator.Board()
+bo.letters
+
+# manuel hoodies
+bo.letters[2][1]    # main man
+bo.letters[2][0]
+bo.letters[2][2]
+
+bo.letters[1][0]
+bo.letters[1][1]
+bo.letters[1][2]
+
+bo.letters[3][0]
+bo.letters[3][1]
+bo.letters[3][2]
+
+
+
+
+try:
+    a = bo.letters[-1][0]
+except:
+    a = None
+
+l = []
+for i in range(1,4):
+    for j in range(1,4):
+        l.append((i,j))
+
+bo.letters[l[1][1]][l[1][0]
+
+bo.hood(1,1)
